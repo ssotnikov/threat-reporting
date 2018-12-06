aec13dd9483b862afae1a01c1f2c982d1db1b114
Igor Egorov <demophobos@gmail.com>
2018-12-06 19:21:01 +0300

    changes

--- TMReportForm/ReportForm.Designer.cs
+++ TMReportForm/ReportForm.Designer.cs
@@ -59,7 +59,7 @@
 			this.reportViewer1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
 			this.reportViewer1.Name = "reportViewer1";
 			this.reportViewer1.ServerReport.BearerToken = null;
-			this.reportViewer1.Size = new System.Drawing.Size(1498, 964);
+			this.reportViewer1.Size = new System.Drawing.Size(1125, 676);
 			this.reportViewer1.TabIndex = 0;
 			this.reportViewer1.WaitControlDisplayAfter = 1;
 			// 
@@ -84,7 +84,7 @@
 			this.toolStrip1.Name = "toolStrip1";
 			this.toolStrip1.Padding = new System.Windows.Forms.Padding(0, 0, 2, 0);
 			this.toolStrip1.RenderMode = System.Windows.Forms.ToolStripRenderMode.Professional;
-			this.toolStrip1.Size = new System.Drawing.Size(1498, 31);
+			this.toolStrip1.Size = new System.Drawing.Size(1125, 31);
 			this.toolStrip1.TabIndex = 1;
 			this.toolStrip1.Text = "toolStrip1";
 			// 
@@ -194,9 +194,9 @@
 			// statusStrip1
 			// 
 			this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
-			this.statusStrip1.Location = new System.Drawing.Point(0, 995);
+			this.statusStrip1.Location = new System.Drawing.Point(0, 707);
 			this.statusStrip1.Name = "statusStrip1";
-			this.statusStrip1.Size = new System.Drawing.Size(1498, 22);
+			this.statusStrip1.Size = new System.Drawing.Size(1125, 22);
 			this.statusStrip1.TabIndex = 2;
 			this.statusStrip1.Text = "statusStrip1";
 			// 
@@ -204,7 +204,7 @@
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-			this.ClientSize = new System.Drawing.Size(1498, 1017);
+			this.ClientSize = new System.Drawing.Size(1125, 729);
 			this.Controls.Add(this.reportViewer1);
 			this.Controls.Add(this.statusStrip1);
 			this.Controls.Add(this.toolStrip1);
